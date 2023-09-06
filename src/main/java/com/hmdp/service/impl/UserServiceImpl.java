fdc8ac16659259a4524a8a5bdf800bff4e004455
lelings <1132235296@qq.com>
2023-09-06 23:13:58 +0800

    登陆部分完成

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,10 +1,17 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -16,5 +23,51 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+
+    private static final String RANDOM_USERNAME_PREFIX = "user_";
+
+    public Result sendCode(String phone, HttpSession session) {
+        if(RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("手机号格式错误");
+        }
+        String code = RandomUtil.randomNumbers(6);
+        session.setAttribute("code",code);
+
+        log.debug("发送短信验证码成功："+code);
+        return Result.ok();
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        String phone = loginForm.getPhone();
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("手机号格式错误");
+        }
+        String cacheCode = session.getAttribute("code").toString();
+        String code = loginForm.getCode();
+        if (cacheCode == null || !cacheCode.equals(code)) {
+            return Result.fail("验证码错误");
+        }
+
+        User user = query().eq("phone",phone).one();
+
+        if (user == null) {
+            user = createUserWithPhone(phone);
+        }
+
+        session.setAttribute("user",user);
+        return Result.ok();
+    }
+
+    private User createUserWithPhone(String phone) {
+        User user = new User();
+        user.setPhone(phone);
+        LocalDateTime now = LocalDateTime.now();
+        user.setCreateTime(now);
+        user.setUpdateTime(now);
+        user.setNickName(RANDOM_USERNAME_PREFIX + RandomUtil.randomString(10));
+        save(user);
+        return user;
+    }
 
 }
